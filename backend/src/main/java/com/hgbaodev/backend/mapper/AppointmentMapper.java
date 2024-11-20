7d4b6870a5fff4ba933b7ff7a9bcbbca227ae8c7
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-11-20 21:33:42 +0700

    feature/hgbaodev/addDb

--- backend/src/main/java/com/hgbaodev/backend/mapper/AppointmentMapper.java
+++ backend/src/main/java/com/hgbaodev/backend/mapper/AppointmentMapper.java
@@ -4,10 +4,14 @@ import com.hgbaodev.backend.dto.response.AppointmentResponse;
 import com.hgbaodev.backend.model.Appointment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring")
 public interface AppointmentMapper {
+
+    AppointmentMapper INSTANCE = Mappers.getMapper(AppointmentMapper.class);
+
     @Mapping(source = "member.memberID", target = "member.memberID")
     AppointmentResponse toAppointmentResponse(Appointment appointment);
 
